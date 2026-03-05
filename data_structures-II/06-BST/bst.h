9b562e5196954ba8640b9c02c05c9dfea529f570
Khushii <155296248+sushi286@users.noreply.github.com>
2026-03-05 15:13:21 +0530

    Update bst.h

--- data_structures-II/06-BST/bst.h
+++ data_structures-II/06-BST/bst.h
@@ -12,199 +12,128 @@
 #ifndef BST_H
 #define BST_H
 
-//*****************************************************************************************************
-
 #include "node.h"
 #include <iostream>
-
-//*****************************************************************************************************
+using namespace std;
 
 template <typename T>
 class BST {
-private:
-    Node<T> *root;
 
-    int _max(int a, int b) const;
-    void _deleteTree(Node<T> *&root);
-    void _insert(Node<T> *&root, const T &item);
-    void _inOrder(Node<T> *root, std::ostream &out) const;
-    void _preOrder(Node<T> *root, std::ostream &out) const;
-    void _postOrder(Node<T> *root, std::ostream &out) const;
-    T *_search(Node<T> *root, const T &item) const;
-    int _height(Node<T> *root) const;
-    // Node<T> *_findMin(Node<T> *root);
+private:
+    Node<T>* root;
+
+    Node<T>* insert(Node<T>* node, const T& value) {
+
+        if(node == nullptr)
+            return new Node<T>(value);
+
+        if(value < node->data)
+            node->left = insert(node->left,value);
+
+        else if(node->data < value)
+            node->right = insert(node->right,value);
+
+        return node;
+    }
+
+    Node<T>* searchNode(Node<T>* node,const T& key) const {
+
+        if(node == nullptr)
+            return nullptr;
+
+        if(key < node->data)
+            return searchNode(node->left,key);
+
+        if(node->data < key)
+            return searchNode(node->right,key);
+
+        return node;
+    }
+
+    void inOrder(Node<T>* node,ostream& out) const {
+
+        if(node==nullptr) return;
+
+        inOrder(node->left,out);
+        out<<node->data<<endl;
+        inOrder(node->right,out);
+    }
+
+    void preOrder(Node<T>* node) const {
+
+        if(node==nullptr) return;
+
+        cout<<node->data<<endl;
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+
+    void postOrder(Node<T>* node) const {
+
+        if(node==nullptr) return;
+
+        postOrder(node->left);
+        postOrder(node->right);
+        cout<<node->data<<endl;
+    }
+
+    int height(Node<T>* node) const {
+
+        if(node==nullptr)
+            return 0;
+
+        int leftHeight = height(node->left);
+        int rightHeight = height(node->right);
+
+        if(leftHeight > rightHeight)
+            return leftHeight+1;
+        else
+            return rightHeight+1;
+    }
 
 public:
-    BST();
-    ~BST();
-    void destroy();
-    void insert(const T &item);
-    void inOrder(std::ostream &out = std::cout) const;
-    void preOrder(std::ostream &out = std::cout) const;
-    void postOrder(std::ostream &out = std::cout) const;
-    T *search(const T &item) const;
-    int height() const;
-};
 
-//*****************************************************************************************************
-
-template <typename T>
-BST<T>::BST() {
-    root = nullptr;
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-BST<T>::~BST() {
-    _deleteTree(root);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-int BST<T>::_max(int a, int b) const {
-    return (a > b) ? a : b;
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::destroy() {
-    _deleteTree(root);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::_deleteTree(Node<T> *&root) {
-    if (root != nullptr) {
-        _deleteTree(root->left);
-        _deleteTree(root->right);
-        delete root;
+    BST() {
         root = nullptr;
     }
-}
 
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::insert(const T &item) {
-    _insert(root, item);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::_insert(Node<T> *&root, const T &item) {
-    if (root == nullptr)
-        root = new Node<T>(item);
-    else if (item < root->value)
-        _insert(root->left, item);
-    else if (item > root->value)
-        _insert(root->right, item);
-    else
-        std::cerr << "\nError: duplicate value\n";
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::inOrder(std::ostream &out) const {
-    _inOrder(root, out);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::_inOrder(Node<T> *root, std::ostream &out) const {
-    if (root != nullptr) {
-        _inOrder(root->left, out);
-        out << root->value << std::endl;
-        _inOrder(root->right, out);
-    }
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::preOrder(std::ostream &out) const {
-    _preOrder(root, out);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::_preOrder(Node<T> *root, std::ostream &out) const {
-    if (root != nullptr) {
-        out << root->value << std::endl;
-        _preOrder(root->left, out);
-        _preOrder(root->right, out);
-    }
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::postOrder(std::ostream &out) const {
-    _postOrder(root, out);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-void BST<T>::_postOrder(Node<T> *root, std::ostream &out) const {
-    if (root != nullptr) {
-        _postOrder(root->left, out);
-        _postOrder(root->right, out);
-        out << root->value << std::endl;
-    }
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-T *BST<T>::search(const T &item) const {
-    return _search(root, item);
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-T *BST<T>::_search(Node<T> *root, const T &item) const {
-    T *result = nullptr;
-
-    if (root != nullptr) {
-        if (item > root->value)
-            result = _search(root->right, item);
-        else if (item < root->value)
-            result = _search(root->left, item);
-        else
-            result = new T(root->value);
+    void insert(const T& value) {
+        root = insert(root,value);
     }
 
-    return result;
-}
+    T* search(const T& key) const {
 
-//*****************************************************************************************************
+        Node<T>* found = searchNode(root,key);
 
-template <typename T>
-int BST<T>::height() const {
-    return _height(root);
-}
+        if(found==nullptr)
+            return nullptr;
 
-//*****************************************************************************************************
+        return new T(found->data);
+    }
 
-template <typename T>
-int BST<T>::_height(Node<T> *root) const {
-    int result = 0;
+    void inOrder() const {
+        inOrder(root,cout);
+    }
 
-    if (root != nullptr)
-        result = 1 + _max(_height(root->left), _height(root->right));
+    void inOrder(ostream& out) const {
+        inOrder(root,out);
+    }
 
-    return result;
-}
+    void preOrder() const {
+        preOrder(root);
+    }
 
+    void postOrder() const {
+        postOrder(root);
+    }
+
+    int height() const {
+        return height(root);
+    }
+
+};
+
+#endif
 //*****************************************************************************************************
 
 // template <typename T>
