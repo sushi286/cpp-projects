920d150bdd52e674fd123cc30872334387fb7df7
Khushii <155296248+sushi286@users.noreply.github.com>
2026-03-05 15:12:22 +0530

    Update node.h

--- data_structures-II/06-BST/node.h
+++ data_structures-II/06-BST/node.h
@@ -7,36 +7,17 @@
 #ifndef NODE_H
 #define NODE_H
 
-//*****************************************************************************************************
-
 template <typename T>
 struct Node {
-    T value;
-    Node<T> *left;
-    Node<T> *right;
+    T data;
+    Node<T>* left;
+    Node<T>* right;
 
-    Node();
-    Node(const T &v, Node<T> *l = nullptr, Node<T> *r = nullptr);
+    Node(const T& value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
 };
-
-//*****************************************************************************************************
-
-template <typename T>
-Node<T>::Node() {
-    value = T();     // T() - default initialization (0 for numbers, empty string for strings, etc.)
-    left = nullptr;
-    right = nullptr;
-}
-
-//*****************************************************************************************************
-
-template <typename T>
-Node<T>::Node(const T &v, Node<T> *l, Node<T> *r) {
-    value = v;
-    left = l;
-    right = r;
-}
-
-//*****************************************************************************************************
 
 #endif
